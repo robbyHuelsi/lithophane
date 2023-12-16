55f7e5b3801e2edf1ca30c11fde3cc829c91b7df
Robert Hülsmann <robert@huelsi.de>
2023-12-16 17:47:33 +0100

    Version 1.0.7

--- src/lithophane/metadata.py
+++ src/lithophane/metadata.py
@@ -4,15 +4,21 @@ __author__ = "Dirk Colbry"
 __authors__ = ["Dirk Colbry", "Robert Hülsmann"]
 
 __project_name__ = "lithophane"
-__version__ = "0.1.0"
+__version__ = "1.0.7"
 __license__ = "MIT"
 __maintainer__ = "Robert Hülsmann"
 __email__ = "robby@huelsi.de"
 __url__ = "https://github.com/robbyHuelsi/lithophane"
 __keywords__ = ["lithophane", "stl", "3d-printing", "3d", "image", "translucent"]
 __classifiers__ = [
-    "Programming Language :: Python :: 3",
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
     "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
 ]
 __python_requires__ = ">=3.8"
